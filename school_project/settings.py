865a27db3bc0e8ae8f1911c37f908c70692c38c2
keita-hayashi <keelmb12.3@gmail.com>
2025-10-21 10:31:16 +0900

    fix STATICFILES settings for render deploy

--- school_project/settings.py
+++ school_project/settings.py
@@ -131,18 +131,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STRAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
-    STATCFILES_DIRS = [
+    STATICFILES_DIRS = [
         BASE_DIR / 'static',
     ]
 
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
