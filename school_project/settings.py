92fb6337f9b1a10e8a36789ec6d545c50d867eff
keita-hayashi <keelmb12.3@gmail.com>
2025-10-22 18:33:04 +0900

    whitenoise-update

--- school_project/settings.py
+++ school_project/settings.py
@@ -141,17 +141,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# 🚨 修正: STATICFILES_DIRS を空のリストとして初期化
+# DEBUG=False の本番環境でこの変数が未定義になることを防ぎます。
+STATICFILES_DIRS = [] 
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+if DEBUG:
+    # DEBUG=Trueの場合のみ、ローカルの静的ファイルフォルダを追加
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+    
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
