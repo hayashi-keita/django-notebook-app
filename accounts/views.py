968c9dd10e630f3428f8d7be0829b368d88098c0
keita-hayashi <keelmb12.3@gmail.com>
2025-10-23 16:41:02 +0900

    ビュー修正2

--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,9 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView, LoginView
 from .models import CustomUser
-from notebook.models import Grade
 from .forms import CustomUserChangeForm, CustomUserCreationForm, CustomPasswordChangeForm, CustomAuthenticationForm, UserSelfUpdateForm
 from .mixins import AdminOnlyMixin, TeacherAndAdminOnlyMixin, UserIsOwnerOrStaffMixin, UserIsOwnerOrAdminMixin
 from django.db.models import Q
-from django.db import DatabaseError
-from django.core.exceptions import ObjectDoesNotExist
 
 # アカウント関連
 class SignUpView(AdminOnlyMixin, CreateView):
@@ -28,7 +25,7 @@ class CustomUserListView(TeacherAndAdminOnlyMixin, ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        queryset = CustomUser.objects.all().select_related('grade', 'classroom').order_by('pk')
+        queryset = CustomUser.objects.all().order_by('pk')
         q = self.request.GET.get('q')
         if q:
             queryset = queryset.filter(
@@ -37,28 +34,13 @@ class CustomUserListView(TeacherAndAdminOnlyMixin, ListView):
         role = self.request.GET.get('role')
         if role:
             queryset = queryset.filter(role=role)
-        grade_pk_param = self.request.GET.get('grade')
-        if grade_pk_param:
-            try:
-                grade_pk = int(grade_pk_param)
-                queryset = queryset.filter(grade=grade_pk)
-            except ValueError:
-                pass
 
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        try:
-            # フィルタリングオプションのために全学年データを取得
-            context['grades'] = Grade.objects.all().order_by('pk')
-        except (DatabaseError, ObjectDoesNotExist) as e:
-            # データベース接続エラーやテーブルが見つからない場合、ログを出力し、クラッシュを防ぐ
-            print(f"ERROR: Failed to fetch Grade objects for context. Check migrations and DB connection: {e}")
-            context['grades'] = [] # 空のリストを渡し、テンプレートの{% for %}ループを安全に通過させる
         context['q'] = self.request.GET.get('q', '')
         context['role'] = self.request.GET.get('role', '')
-        context['grade'] = self.request.GET.get('grade', '')
         return context
 
 class CustomUserDetailView(UserIsOwnerOrStaffMixin, DetailView):
