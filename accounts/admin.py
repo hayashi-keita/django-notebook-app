c94cd3fe15f9d106e5bae12d7ff2e8445283f6ec
keita-hayashi <keelmb12.3@gmail.com>
2025-10-23 14:49:54 +0900

    ユーザー一覧修正

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,8 +4,8 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        ('追加情報', {'fields': ('role', 'grade', 'classroom', 'full_name', 'gender')}),
+        ('追加情報', {'fields': ('role', 'grade', 'classroom', 'full_name', 'student_id', 'gender')}),
     )
-    list_display = ('username', 'email', 'role', 'grade', 'classroom', 'full_name', 'gender')
+    list_display = ('username', 'email', 'role', 'grade', 'classroom', 'full_name', 'student_id', 'gender')
 
 admin.site.register(CustomUser, CustomUserAdmin)
